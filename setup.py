7c2ad84e31a57bf6030fda6cb498597b5a3c1551
Alex Gorodnitskiy <alekc1272@gmail.com>
2021-07-02 18:31:22 +0300

    (-) fix find_packages

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         url="https://github.com/gorodnitskiy/advanced_parser",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        packages=find_packages(),
+        packages=find_packages('advanced_argparse'),
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
